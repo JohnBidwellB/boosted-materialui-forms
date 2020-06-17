d37742b02a6615ef9e9feb6df52fe84301c9deb0
John Bidwell <john@foreach.cl>
2020-06-16 21:00:20 -0400

    tests updated

--- src/components/FormField.test.js
+++ src/components/FormField.test.js
@@ -4,7 +4,7 @@ import FormField from "components/FormField";
 
 describe("FormField", () => {
   test("should display label", () => {
-    const { container } = render(<FormField label="Test label" />);
+    const { container } = render(<FormField config={{ label:"Test label" }} />);
     getAllByText(container, "Test label");
   });
 });

90f633d8bd869e432b887b1be15e4dbb04c0181a
John Bidwell <johnbidwellb@gmail.com>
2020-10-16 17:51:37 -0300

    fixed phone validations

--- src/validations/cellphone.validation.js
+++ src/validations/cellphone.validation.js
@@ -1,4 +1,5 @@
 export const chileanCellphoneValidation = (value) => {
-	const regularExpression = /^(\+?56)?(\s?)(0?9)(\s?)[9876543]\d{7}$/;
-	return regularExpression.test(value);
+	const cellphone = /^(\+?56)?(\s?)(0?9)(\s?)[9876543]\d{7}$/;
+	const fixedphone = /^((\+?56\s)?(0?2|0?3[2-5]|0?4[1-5]|0?5[123578]|0?6[13457]|0?7[1235])?(\s2\d{6}|\s\d{6}))$/;
+	return cellphone.test(value) || fixedphone.test(value);
 };
